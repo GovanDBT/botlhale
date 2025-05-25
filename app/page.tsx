ea279a1c5dbbaa3216c277ea93da5dbce81ea9aa
GovanDBT <govan.dibotelo@gmail.com>
2025-05-25 15:23:37 +0200

    add authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import LoginForm from "./components/LoginForm";
+import LoginPage from "./login/page";
 import Schools from "./schools/page";
 
 export default function Home() {
   return (
     <div>
-      <Schools />
+      <LoginPage />
     </div>
   );
 }
