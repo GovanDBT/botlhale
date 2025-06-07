1edc39a58aeb62b8a81e5f07b8eb0bbd9ea176db
GovanDBT <govan.dibotelo@gmail.com>
2025-06-07 23:29:26 +0200

    add a dynamic button to layout

--- app/dashboard/admin/users/students/page.tsx
+++ app/dashboard/admin/users/students/page.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+"use client";
+import { useContext, useEffect } from "react";
+
+import layoutButtonContext from "@/app/context/layoutButtonContext";
 
 const StudentsPage = () => {
+  const { setButtonTitle, setButtonLink } = useContext(layoutButtonContext); // our button context
+
+  // effect hook for setting the button link and title and unmounting when done
+  useEffect(() => {
+    setButtonTitle("Add Student");
+    setButtonLink("#");
+
+    return () => {
+      setButtonTitle("");
+      setButtonLink("");
+    };
+  }, [setButtonLink, setButtonTitle]);
+
   return <div>StudentsPage</div>;
 };
 
