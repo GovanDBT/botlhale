cb96fd0ea14d0914fa61ff77f66b5c1bd976c038
GovanDBT <govan.dibotelo@gmail.com>
2025-06-18 00:56:12 +0200

    update navbar links

--- app/(landing)/Navbar.tsx
+++ app/(landing)/Navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Lato } from "next/font/google";
 
 import {
   NavigationMenu,
@@ -14,16 +15,21 @@ import {
 } from "@/components/ui/navigation-menu";
 import AppButton from "../components/AppButton";
 
+const lato = Lato({
+  weight: "400",
+});
+
 const Navbar = () => {
   const pathname = usePathname(); // Get the current path
+
   return (
-    <nav className="flex justify-between items-center py-5">
+    <nav className={`flex justify-between items-center py-5 ${lato.className}`}>
       <Link href="/">
         <Image src="/Strype.svg" alt="Strype logo" width={220} height={220} />
       </Link>
       <NavigationMenu viewport={false}>
         <NavigationMenuList className="space-x-10">
-          <NavigationMenuItem className="px-5">
+          <NavigationMenuItem>
             <NavigationMenuLink asChild>
               <Link
                 href="/"
@@ -32,6 +38,18 @@ const Navbar = () => {
                 } hover:text-primary`}
               >
                 Home
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link
+                href="#"
+                className={`${
+                  pathname === "/register" && "font-bold text-primary"
+                } hover:text-primary`}
+              >
+                About
               </Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
@@ -76,24 +94,54 @@ const Navbar = () => {
             </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link href="#">Solutions</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link href="#">Pricing</Link>
-            </NavigationMenuLink>
+            <NavigationMenuTrigger>Rankings</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <li className="row-span-3">
+                  <NavigationMenuLink asChild>
+                    <a
+                      className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-6 no-underline outline-hidden select-none focus:shadow-md"
+                      href="/"
+                    >
+                      <div className="mt-4 mb-2 text-lg font-medium">
+                        shadcn/ui
+                      </div>
+                      <p className="text-muted-foreground text-sm leading-tight">
+                        Beautifully designed components built with Tailwind CSS.
+                      </p>
+                    </a>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <Link href="#">
+                    <span className="block font-bold">Introduction</span>
+                    Re-usable components built using Radix UI and Tailwind CSS.
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#">
+                    <span className="block font-bold">Introduction</span>
+                    Re-usable components built using Radix UI and Tailwind CSS.
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#">
+                    <span className="block font-bold">Introduction</span>
+                    Re-usable components built using Radix UI and Tailwind CSS.
+                  </Link>
+                </li>
+              </ul>
+            </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink asChild>
               <Link
-                href="/register"
+                href="#"
                 className={`${
                   pathname === "/register" && "font-bold text-primary"
                 } hover:text-primary`}
               >
-                Register school
+                Statistics
               </Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
