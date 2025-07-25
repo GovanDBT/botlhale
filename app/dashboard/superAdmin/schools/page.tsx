31c991e144a9ad25bb3f74699e9c8933290cb7a9
GovanDBT <govan.dibotelo@gmail.com>
2025-07-25 23:23:34 +0200

    make school table data reusable

--- app/dashboard/superAdmin/schools/page.tsx
+++ app/dashboard/superAdmin/schools/page.tsx
@@ -1,24 +1,47 @@
 "use client";
+
 import AppSeparator from "@/app/components/AppSeparator";
 import AppInfoTooltip from "@/app/components/AppInfoTooltip";
-import SchoolTable from "../components/SchoolTable";
+import { DataTable } from "../../components/DataTable";
+import { columns } from "./TableColumn";
 import SchoolFormSheet from "../components/SchoolFormSheet";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const SchoolsPage = () => {
+  // Fetch data using react-query
+  const fetchSchools = async () => {
+    const { data } = await axios.get("/api/schools/profile");
+    return data;
+  };
+
+  const {
+    data: schools = [],
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["schools"],
+    queryFn: fetchSchools,
+  });
+
   return (
-    <>
-      <div>
-        <div className="flex justify-between items-center">
-          <div className="flex gap-2 items-center">
-            <h1>Schools</h1>
-            <AppInfoTooltip content="A list of all registered Schools" />
-          </div>
-          <SchoolFormSheet />
+    <div>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+          <h1>Schools</h1>
+          <AppInfoTooltip content="A list of all registered Schools" />
         </div>
-        <AppSeparator />
-        <SchoolTable />
+        <SchoolFormSheet />
       </div>
-    </>
+      <AppSeparator />
+
+      <DataTable
+        columns={columns}
+        data={schools}
+        isLoading={isLoading}
+        error={error ? "Failed to fetch schools" : null}
+      />
+    </div>
   );
 };
 
