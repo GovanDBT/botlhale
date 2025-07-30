91d444ec61d972a1aaa676fbe7dc8ec648805bb7
GovanDBT <govan.dibotelo@gmail.com>
2025-07-30 16:14:08 +0200

    remove trash files

--- app/(landing)/change-password/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Change your password</div>;
-};
-
-export default page;
--- app/schools/page.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@/services/supabase/server";
-
-export default async function Schools() {
-  const supabase = await createClient();
-  const { data: schools } = await supabase.from("Schools").select();
-
-  return <pre>{JSON.stringify(schools, null, 2)}</pre>;
-}
