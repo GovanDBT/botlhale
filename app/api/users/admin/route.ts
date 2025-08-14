ae1cf189dd08c7321b6b2c65a8762fe92e9fcdc7
GovanDBT <govan.dibotelo@gmail.com>
2025-08-14 15:31:21 +0200

    create admin via email invite

--- app/api/users/admin/route.ts
+++ app/api/users/admin/route.ts
@@ -2,12 +2,13 @@
 // admin response api's
 import { getUserRole } from "@/utils/getUserRole";
 import { NextRequest, NextResponse } from "next/server";
-import { serverInstance } from "@/services/rollbar/rollbar";
 import { adminSchema } from "@/lib/validationSchema";
 import { createClient } from "@/services/supabase/server";
 import { generateUserId } from "@/lib/generateUserId";
+import * as Sentry from "@sentry/nextjs";
 import { adminAuthClient } from "@/services/supabase/admin";
 
+// (POST/admin) create new admin
 export async function POST(request: NextRequest) {
   try {
     // create a new body request
@@ -31,7 +32,7 @@ export async function POST(request: NextRequest) {
 
       // if api fails to retrieve user session
       if (sessionError) {
-        serverInstance.info(`Session error: ${sessionError.message}`);
+        Sentry.captureException(`Session error: ${sessionError.message}`)
         return NextResponse.json(
           { error: `Session error: ${sessionError.message}` || "Failed to fetch user session" },
           { status: sessionError.status }
@@ -43,7 +44,7 @@ export async function POST(request: NextRequest) {
     
       // if no access token was provided
       if (!accessToken) {
-        serverInstance.info("System failed to provide access token");
+        Sentry.captureMessage("System fails to provide access token", "error")
         return NextResponse.json(
           { error: "No access token provided" },
           { status: 409 }
@@ -56,10 +57,7 @@ export async function POST(request: NextRequest) {
 
     // If user is not superAdmin, throw an error
     if (userRole !== "superAdmin") {
-      serverInstance.warning("An unauthorized users tried to create admin user", {
-        accessToken,
-        userRole,
-      });
+      Sentry.captureMessage("An unauthorized users tried to create admin user", "warning")
       return NextResponse.json(
         { error: "Unauthorized access!" },
         { status: 403 }
@@ -86,9 +84,9 @@ export async function POST(request: NextRequest) {
 
     // if system fails to verify user data
     if (userError) {
-      serverInstance.error(`Verify User Profile Error: ${userError.message}`);
+      Sentry.captureException(`Profile Error: ${userError.message}`)
       return NextResponse.json(
-        { error: `Verify User Profile Error: ${userError.message}` || "Failed to fetch user profile" },
+        { error: `Profile Error: ${userError.message}` || "Failed to fetch user profile" },
         { status: 404 }
       );
     }
@@ -118,7 +116,7 @@ export async function POST(request: NextRequest) {
 
     // if system fails to verify school
     if (schoolError) {
-      serverInstance.error(`School Error: ${schoolError.message}`);
+      Sentry.captureException(`School Error: ${schoolError.message}`);
       return NextResponse.json(
         { error: `School Error: ${schoolError.message}` || "Failed to fetch school id" },
         { status: 500 }
@@ -127,7 +125,7 @@ export async function POST(request: NextRequest) {
 
     // if school ID does not exist
     if (!school) {
-      serverInstance.info("School ID that does not exist was submitted");
+      Sentry.captureMessage("School does not exist", "warning");
       return NextResponse.json(
         { error: "School does not exist" },
         { status: 404 }
@@ -139,110 +137,64 @@ export async function POST(request: NextRequest) {
 
     // if user ID not generated
     if (!userId) {
-      serverInstance.error("System failed to generate user ID");
+      Sentry.captureMessage("System failed to generate user ID", "fatal")
       return NextResponse.json(
         { error: "Failed to generate user ID. Please try again later." },
         { status: 500 }
       );
     }
 
-    // ----------- Begin pseudo-transaction -----------
-    
-    let createdAuthUserId;
+    // invite new user via email
+    let { data: invitedUser, error: inviteError } = await adminAuthClient.inviteUserByEmail(body.email, { 
+      redirectTo: "http://localhost:3000/reset-password",
+      data: {
+        firstname: body.firstname,
+        lastname: body.lastname,
+        school: body.school,
+        email: body.email,
+        phone: body.phone,
+        profile_role: "admin",
+        profile_id: userId,
+        profile_status: "invited",
+        display_name: `${body.firstname} ${body.lastname}`
+      },
+    });
 
-    try {
-      let { error: inviteError } = await adminAuthClient.inviteUserByEmail(body.email, { 
-        redirectTo: "http://localhost:3000/reset-password",
-        data: {
-          firstname: body.firstname,
-          lastname: body.lastname,
-          user_id: userId,
-          school: body.school_id,
-          email: body.email,
-          phone: body.phone,
-          role: "admin",
-          user_status: "invited",
-          display_name: `${body.firstname} ${body.lastname}`
-        },
-      });
-
-      if (inviteError) {
-        throw new Error(inviteError.message || "Failed to send invite email");
-      }
-      // register user
-      // let { data: newUser, error: newUserError } = await adminAuthClient.createUser({
-      //   phone: body.phone,
-      //   email: body.email,
-      //   password: userId,
-      //   email_confirm: true,
-      //   user_metadata: {
-      //     display_name: body.firstname + " " + body.lastname
-      //   }
-      // });
-    
-      // fails to register user
-      // if (newUserError) {
-      //   throw new Error(newUserError.message || "Failed to signup user");
-      // }
-
-      // createdAuthUserId = invite.user?.id;
-    
-      // create profile
-      // const { error: profileError } = await supabase
-      //   .from("profile")
-      //   .insert([
-      //     {
-      //       id: createdAuthUserId,
-      //       firstname: body.firstname,
-      //       lastname: body.lastname,
-      //       email: body.email,
-      //       phone: body.phone,
-      //       user_id: userId,
-      //       school_id: body.school_id,
-      //       role: "admin",
-      //       user_status: "invited",
-      //     },
-      //   ])
-      //   .select();
-    
-      // fails to create profile
-      // if (profileError) {
-      //   throw new Error(profileError.message || "Failed to create user profile");
-      // }
-    
-      // send reset password email
-      // const { error: resetPasswordError } = await supabase.auth
-      //   .resetPasswordForEmail(body.email, { redirectTo: 'http://localhost:3000/reset-password' });
-
-      // if (resetPasswordError) {
-      //   throw new Error(resetPasswordError.message || "Failed to send reset password email");
-      // }
-
-      // send success response
+    // if invite fails
+    if (inviteError) {
+      Sentry.captureException(`Invite Error: ${inviteError.message}`)
       return NextResponse.json(
-        { message: "Admin successfully registered" },
-        { status: 201 }
-      );
-      
-    } catch (error: any) {
-      // Rollback: delete created Auth user if something failed
-      if (createdAuthUserId) {
-        await adminAuthClient.deleteUser(createdAuthUserId);
-      }
-
-      return NextResponse.json(
-        { error: `User Registration Error: ${error.message}` },
-        { status: 500 }
+        { success: false, error: `Invite Error: ${inviteError.message}` || "Failed to send invite" },
+        { status: inviteError.status }
       );
     }
 
-    // ----------- End pseudo-transaction -----------
+    // update the phone field in auth.users
+    if (invitedUser?.user?.id) {
+      const { error: updateError } = await adminAuthClient.updateUserById(invitedUser.user.id, {
+        phone: body.phone
+      });
 
+      // if phone update fails
+      if (updateError) {
+        Sentry.captureException(`Auth Phone Update Error: ${updateError.message}`)
+        return NextResponse.json(
+          { success: false, error: `Auth Phone Update Error: ${updateError.message}` || "Failed to update phone in auth.users" },
+          { status: updateError.status }
+        );
+      }
+    }
+
+    // send success response
+    return NextResponse.json(
+      { message: "Admin successfully registered" },
+      { status: 201 }
+    );
   } catch (error: any) {
-    serverInstance.critical("System failed to log in a user", error);
+    Sentry.captureException(`Post Admin Server Error: ${error.message}`)
     return NextResponse.json(
       { success: false, error: `Server error: ${error.message}`, },
-      { status: 500 }
+      { status: error.status }
     );
   }
   
